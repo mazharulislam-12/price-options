a361773fa959567cd5bcd9445c4ee9cea0d90db3
mazharulislam-12 <505mazharulislam@gmail.com>
2023-09-17 23:33:45 -0700

    naber create and resopnsive

--- src/Components/Navber/Navbar.jsx
+++ src/Components/Navber/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { AiOutlineMenu,AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
   const routes = [
     { path: '/', id: 1, name: 'Home' },
     { path: '/about', id: 2, name: 'About' },
@@ -10,8 +13,16 @@ const Navbar = () => {
   ];
 
   return (
-    <nav>
-      <ul className="md:flex">
+    <nav className="text-black bg-yellow-200 p-6">
+        <div className="md:hidden text-2xl" onClick={()=> setOpen(!open)}>
+            {
+                open === true ? 
+                <AiOutlineClose></AiOutlineClose> 
+                : <AiOutlineMenu className=" "></AiOutlineMenu>
+
+            }
+        </div>
+      <ul className={`md:flex duration-1000 absolute md:static bg-yellow-200 px-6  ${open?'top-16':'-top-60'} `}>
       {
         routes.map(route => <Link key={route.id} route={route} ></Link>)
       }
